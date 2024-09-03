e5a0fa6af58501c7496fc38e767172b79aac2bb8
Narci <yanatsy002@gmail.com>
2024-09-03 23:22:24 +0200

    add all routes

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLAlALCHEMY_DATABASE_URI'] = 'sqlite:///subtitles.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///subtitles.db'
 db = SQLAlchemy(app)
 
-class Subtitles(db.Model):
+class Subtitle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sequence_number = db.Column(db.Integer, nullable=False)
     text = db.Column(db.Text, nullable=False)
@@ -14,32 +14,86 @@ class Subtitles(db.Model):
     def __repr__(self):
         return f'<Subtitle {self.id}: {self.text[:20]}...>'
 
+# subtitles tracking:
+current_subtitle_index = 0
+
+
 # get subtitle
 @app.route('/api/subtitles/next', methods=['GET'])
 def get_next_subtitle():
-    current_subtitle = Subtitle.query.order_by(Subtitle.sequence_number).first()
-    if current_subtitle:
+    global current_subtitle_index
+    
+    subtitles = Subtitle.query.order_by(Subtitle.sequence_number).all()
+    
+    if current_subtitle_index < len(subtitles):
+
+        current_subtitle = subtitles[current_subtitle_index]
+
         return jsonify({
             'id': current_subtitle.id,
             'text': current_subtitle.text,
-            'language': current_subtitle.language
+            'language': current_subtitle.language,
+            'sequence_number': current_subtitle.sequence_number
         })
     else:
-        return jsonify({'message': 'No subtitles available'}),
+        return jsonify({'message': 'No more subtitles available'}), 404
+
 
 @app.route('/api/subtitles/advance', methods=['Post'])
 def advance_subtitle():
-    pass
+    global current_subtitle_index
+    current_subtitle_index += 1
+
+    subtitles = Subtitle.query.order_by(Subtitle.sequence_number).all()
+    
+    if current_subtitle_index < len(subtitles):
+
+        return jsonify({'message': 'Advanced to next subtitle'}), 200
+    else:
+
+        current_subtitle_index = len(subtitles) - 1
+        return jsonify({'message': 'Reached the end of subtitles'}), 200
+
 
 # add new subtitle
 @app.route('/api/subtitles', methods=['POST'])
 def add_subtitle():
-    pass
+    data = request.json
+
+    if not all(key in ('text', 'language', 'sequence_number')):
+        return jsonify({'message': 'Missing required fields'}), 400
+    
+    new_subtitle = Subtitle(
+        text=data['text'],
+        language=data['language'],
+        sequence_number=data['sequence_number']
+    )
+
+    db.session.add(new_subtitle)
+    db.session.commit()
+
+    return jsonify({
+        'id': new_subtitle.id,
+        'text': new_subtitle.text,
+        'language': new_subtitle.language,
+        'sequence_number': new_subtitle.sequence_number
+    }), 201  
 
 # all subtitless
 @app.route('/api/subtitles', methods=['GET'])
 def get_all_subtitles():
-    pass
+ 
+    subtitles = Subtitle.query.order_by(Subtitle.sequence_number).all()
+    
+    subtitles_list = [{
+        'id': subtitle.id,
+        'text': subtitle.text,
+        'language': subtitle.language,
+        'sequence_number': subtitle.sequence_number
+    } for subtitle in subtitles]
+
+    return jsonify(subtitles_list)
+
 
 @app.route('/')
 def home():
@@ -47,4 +101,7 @@ def home():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+    
